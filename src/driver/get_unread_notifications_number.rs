78502b83890f9bf020a88e202c922988d4220876
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2024-04-21 10:55:32 +0100

    Add statement that return zero notifications if element not found
    Added if statement, when using the option 'un', to return 0 if there is no notifications

--- src/driver/get_unread_notifications_number.rs
+++ src/driver/get_unread_notifications_number.rs
@@ -7,14 +7,14 @@ pub async fn get_unread_notifications_number(driver: &WebDriver) {
         .await
         .unwrap();
 
-    let notif_text: String = notifications_dash
-        .query(By::Css("strong"))
-        .first()
-        .await
-        .unwrap()
-        .text()
-        .await
-        .unwrap();
+    let notif_element = notifications_dash.query(By::Css("strong"));
+
+    if notif_element.not_exists().await.unwrap() {
+        println!("0");
+        return;
+    }
+
+    let notif_text: String = notif_element.first().await.unwrap().text().await.unwrap();
 
     let notif_num = notif_text.split_whitespace().collect::<Vec<&str>>()[1];
 
